e431be2fc786d8418d18ee97d093f118413346cd
kimxogus <kgyoo8232@gmail.com>
2016-10-15 21:39:27 +0900

    fix all errors

--- android/src/main/java/io/xogus/reactnative/versioncheck/RNVersionCheckModule.java
+++ android/src/main/java/io/xogus/reactnative/versioncheck/RNVersionCheckModule.java
@@ -3,6 +3,7 @@ package io.xogus.reactnative.versioncheck;
 
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
@@ -13,87 +14,125 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
-public class RNVersionCheckModule extends ReactContextBaseJavaModule {
+import javax.annotation.Nullable;
 
+public class RNVersionCheckModule extends ReactContextBaseJavaModule
+{
     private final ReactApplicationContext reactContext;
+    private final String packageName;
 
-    public RNVersionCheckModule(ReactApplicationContext reactContext) {
+    public RNVersionCheckModule(ReactApplicationContext reactContext)
+    {
         super(reactContext);
         this.reactContext = reactContext;
+        this.packageName = this.reactContext.getPackageName();
     }
+
 
     @ReactMethod
-    public void getCurrentVersion(Promise promise) {
-        final String packageName = this.reactContext.getPackageName();
-        PackageManager packageManager = this.reactContext.getPackageManager();
-        try {
-            PackageInfo info = packageManager.getPackageInfo(packageName, 0);
-            promise.resolve(info.versionName);
-        } catch (PackageManager.NameNotFoundException e) {
-            promise.reject("NAME_NOT_FOUND", "Package name is not found.");
-        }
-    }
-
-    @ReactMethod
-    public void getLatestVerion(Promise promise) {
-        final String packageName = this.reactContext.getPackageName();
-        try {
-            promise.resolve(getMarketVersion(packageName));
-        } catch (Exception e) {
-            promise.reject("PARSING_ERROR", "Parsing the latest app version failed.");
-        }
-    }
-
-    private String getMarketVersion(String packageName) throws Exception {
-        String mData = "", mVer = null;
-
-        URL mUrl = new URL("https://play.google.com/store/apps/details?id="
-                + packageName);
-        HttpURLConnection mConnection = (HttpURLConnection) mUrl
-                .openConnection();
-
-        if (mConnection == null)
-            return null;
-
-        mConnection.setConnectTimeout(5000);
-        mConnection.setUseCaches(false);
-        mConnection.setDoOutput(true);
-
-        if (mConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {
-            BufferedReader mReader = new BufferedReader(
-                    new InputStreamReader(mConnection.getInputStream()));
-
-            while (true) {
-                String line = mReader.readLine();
-                if (line == null)
-                    break;
-                mData += line;
-            }
-
-            mReader.close();
-        }
-
-        mConnection.disconnect();
-
-        String startToken = "softwareVersion\">";
-        String endToken = "<";
-        int index = mData.indexOf(startToken);
-
-        if (index == -1) {
-            mVer = null;
-
-        } else {
-            mVer = mData.substring(index + startToken.length(), index
-                    + startToken.length() + 100);
-            mVer = mVer.substring(0, mVer.indexOf(endToken)).trim();
-        }
-
-        return mVer;
+    public void getLatestVersion(Promise promise)
+    {
+        new ParseMarket(promise).execute(packageName);
     }
 
     @Override
-    public String getName() {
+    public @Nullable Map<String, Object> getConstants()
+    {
+        Map<String, Object> constants = new HashMap<>();
+
+        constants.put("packageName", packageName);
+
+        PackageManager packageManager = this.reactContext.getPackageManager();
+        try
+        {
+            PackageInfo info = packageManager.getPackageInfo(packageName, 0);
+            constants.put("currentVersion", info.versionName);
+            constants.put("currentBuildNumber", info.versionCode);
+        }
+        catch (PackageManager.NameNotFoundException e)
+        {
+            constants.put("currentVersion", null);
+            constants.put("currentBuildNumber", null);
+        }
+
+        return constants;
+    }
+
+    @Override
+    public String getName()
+    {
         return "RNVersionCheck";
+    }
+
+
+    private class ParseMarket extends AsyncTask<String, Integer, String>
+    {
+
+        private Promise promise;
+
+        private ParseMarket(Promise promise)
+        {
+            this.promise = promise;
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            String packageName = params[0];
+            StringBuilder mData = new StringBuilder();
+
+            try {
+                URL mUrl = new URL("https://play.google.com/store/apps/details?id=" + packageName);
+                HttpURLConnection mConnection = (HttpURLConnection) mUrl.openConnection();
+
+                if (mConnection == null)
+                    return null;
+
+                mConnection.setConnectTimeout(5000);
+                mConnection.setUseCaches(false);
+                mConnection.setDoOutput(true);
+
+                if (mConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                    BufferedReader mReader = new BufferedReader(new InputStreamReader(mConnection.getInputStream()));
+
+                    String line;
+                    while (true) {
+                        line = mReader.readLine();
+
+                        if (line == null) break;
+
+                        mData.append(line);
+                    }
+
+                    mReader.close();
+                }
+
+                mConnection.disconnect();
+            } catch (Exception e) {
+                promise.reject("PARSE_ERROR", "Parsing market failed.");
+            }
+
+            String mVer;
+
+            String startToken = "softwareVersion\">";
+            String endToken = "<";
+            int index = mData.indexOf(startToken);
+
+            if (index == -1)
+            {
+                mVer = null;
+            }
+            else
+            {
+                mVer = mData.substring(index + startToken.length(), index + startToken.length() + 100);
+                mVer = mVer.substring(0, mVer.indexOf(endToken)).trim();
+            }
+
+            promise.resolve(mVer);
+
+            return null;
+        }
     }
 }
--- index.js
+++ index.js
@@ -1,50 +1,45 @@
-import { NativeModules } from 'react-native';
+import { NativeModules, Platform } from 'react-native';
 
 const { RNVersionCheck } = NativeModules;
 
-const getCurrentVersion = RNVersionCheck.currentVersion;
-const getLatestVersion = RNVersionCheck.latestVersion;
+const PACKAGE_NAME = RNVersionCheck.packageName;
+const CURRENT_BUILD_NUMBER = RNVersionCheck.currentBuildNumber;
+const CURRENT_VERSION = RNVersionCheck.currentVersion;
+const getLatestVersionNative = RNVersionCheck.getLatestVersion;
 
+let latestVersion;
 
-export default {
-    getCurrentVersion,
-    getLatestVersion,
-    needUpdate: (depth = Infinity, delimiter = '.') => {
-        if(typeof depth === 'string') {
-            delimiter = depth;
-            depth = Infinity;
-        }
-
-        let currentVersion, latestVersion;
-
-        return Promise.resolve()
-
-            .then(getCurrentVersion)
-            .then((ver) => {
-                currentVersion = ver;
+const getLatestVersion = () => {
+    if(latestVersion) {
+        return Promise.resolve(latestVersion);
+    } else {
+        return getLatestVersionNative()
+            .then((version) => {
+                latestVersion = version;
             })
+            .catch(() => fetch("https://play.google.com/store/apps/details?id=" + PACKAGE_NAME, { timeout: 5000 }))
+            .then(res => res.text())
+            .then((text) => {
+                const startToken = "softwareVersion\">";
+                const endToken = "<";
 
-            .then(getLatestVersion)
-            .then((ver) => {
-                latestVersion = ver;
-            })
+                const indexStart = text.indexOf(startToken);
 
-            .then(() => {
-                currentVersion = getVersion(currentVersion, depth, delimiter);
-                latestVersion = getVersion(latestVersion, depth, delimiter);
+                if (indexStart == -1) {
+                    return Promise.reject();
+                } else {
+                    text = text.substr(indexStart + startToken.length);
+                    text = text.substr(0, text.indexOf(endToken));
 
-                for(let i = 0; i < depth; i++) {
-                    if(latestVersion[i] > currentVersion[i]) {
-                        return Promise.resolve(true);
-                    }
+                    latestVersion = text.trim();
+                    return Promise.resolve(latestVersion);
                 }
-                return Promise.resolve(false);
             });
     }
 };
 
 
-function getVersion(version, depth, delimiter) {
+function getVersionNumberArray(version, depth, delimiter) {
     version = String(version);
 
     if(version.indexOf(delimiter) == -1) {
@@ -60,3 +55,38 @@ function getVersion(version, depth, delimiter) {
         return result;
     }
 }
+
+export default {
+    getPackageName: () => PACKAGE_NAME,
+    getCurrentBuildNumber: () =>CURRENT_BUILD_NUMBER,
+    getCurrentVersion: () => CURRENT_VERSION,
+    getLatestVersion: getLatestVersion,
+    needUpdate: (depth = Infinity, delimiter = '.') => {
+        if(typeof depth === 'string') {
+            delimiter = depth;
+            depth = Infinity;
+        }
+
+        let currentVersion = CURRENT_VERSION;
+
+        return Promise.resolve()
+            .then(() => getLatestVersion())
+            .then((latestVersion) => {
+                currentVersion = getVersionNumberArray(currentVersion, depth, delimiter);
+                latestVersion = getVersionNumberArray(latestVersion, depth, delimiter);
+
+                for(let i = 0; i < depth; i++) {
+                    if(!latestVersion[i] && !currentVersion[i]) {
+                        return Promise.resolve(false);
+                    }
+                    if(!currentVersion[i]) {
+                        return Promise.resolve(true);
+                    }
+                    if(latestVersion[i] > currentVersion[i]) {
+                        return Promise.resolve(true);
+                    }
+                }
+                return Promise.resolve(false);
+            });
+    }
+};
