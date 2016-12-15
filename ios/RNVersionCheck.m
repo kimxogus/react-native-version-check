c3cf685e005d31e511a6355d165094c880033f2a
kimxogus <kgyoo8232@gmail.com>
2016-12-15 23:06:09 +0900

    add ios native info

--- ios/RNVersionCheck.m
+++ ios/RNVersionCheck.m
@@ -9,5 +9,41 @@
 }
 RCT_EXPORT_MODULE()
 
+
+- (NSString*) country
+{
+    NSString *country = [[NSLocale currentLocale] objectForKey:NSLocaleCountryCode];
+    return country;
+}
+
+- (NSString*) packageName
+{
+    return [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleIdentifier"];
+}
+
+- (NSString*) currentVersion
+{
+    return [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleShortVersionString"];
+}
+
+- (NSString*) currentBuildNumber
+{
+    return [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleVersion"];
+}
+
+
+- (NSDictionary *)constantsToExport
+{
+    UIDevice *currentDevice = [UIDevice currentDevice];
+    
+    NSString *uniqueId = [DeviceUID uid];
+    
+    return @{
+             @"country": country,
+             @"packageName": packageName,
+             @"currentVersion": currentVersion,
+             @"currentBuildNumber": currentBuildNumber,
+             };
+}
+
 @end
-  
