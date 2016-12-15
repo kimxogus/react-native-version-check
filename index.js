805030dd3a0ff1089ffffa8113b8fd9143ec4a91
kimxogus <kgyoo8232@gmail.com>
2016-12-16 00:41:42 +0900

    add getCountry

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import { NativeModules } from "react-native";
 
 const { RNVersionCheck } = NativeModules;
 
+const COUNTRY = RNVersionCheck.country;
 const PACKAGE_NAME = RNVersionCheck.packageName;
 const CURRENT_BUILD_NUMBER = RNVersionCheck.currentBuildNumber;
 const CURRENT_VERSION = RNVersionCheck.currentVersion;
@@ -94,6 +95,7 @@ function needUpdate (depth = Infinity, delimiter = ".") {
 }
 
 export default {
+  getCountry: () => COUNTRY,
   getPackageName: () => PACKAGE_NAME,
   getCurrentBuildNumber: () => CURRENT_BUILD_NUMBER,
   getCurrentVersion: () => CURRENT_VERSION,
