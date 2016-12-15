614d2e9b818137a37e17f8f85d086d4991a6492d
kimxogus <kgyoo8232@gmail.com>
2016-12-15 23:27:29 +0900

    change settings

--- index.js
+++ index.js
@@ -10,93 +10,93 @@ const getLatestVersionNative = RNVersionCheck.getLatestVersion;
 let latestVersion;
 
 const getLatestVersion = () => {
-    if (latestVersion) {
+  if (latestVersion) {
+    return Promise.resolve(latestVersion);
+  } else {
+    return getLatestVersionNative()
+      .then((version) => {
+        latestVersion = version;
         return Promise.resolve(latestVersion);
-    } else {
-        return getLatestVersionNative()
-            .then((version) => {
-                latestVersion = version;
-                return Promise.resolve(latestVersion);
-            })
-            .catch(() => {
-                return fetch("https://play.google.com/store/apps/details?id=" + PACKAGE_NAME, { timeout: 5000 })
-                    .then(res => {
-                        return res.text();
-                    })
-                    .then((text) => {
-                        const startToken = "softwareVersion\">";
-                        const endToken = "<";
+      })
+      .catch(() => {
+        return fetch("https://play.google.com/store/apps/details?id=" + PACKAGE_NAME, { timeout: 5000 })
+          .then(res => {
+            return res.text();
+          })
+          .then((text) => {
+            const startToken = "softwareVersion\">";
+            const endToken = "<";
 
-                        const indexStart = text.indexOf(startToken);
+            const indexStart = text.indexOf(startToken);
 
-                        if (indexStart == -1) {
-                            return Promise.reject();
-                        } else {
-                            text = text.substr(indexStart + startToken.length);
-                            text = text.substr(0, text.indexOf(endToken));
+            if (indexStart == -1) {
+              return Promise.reject();
+            } else {
+              text = text.substr(indexStart + startToken.length);
+              text = text.substr(0, text.indexOf(endToken));
 
-                            latestVersion = text.trim();
-                            return Promise.resolve(latestVersion);
-                        }
-                    });
-            });
-    }
+              latestVersion = text.trim();
+              return Promise.resolve(latestVersion);
+            }
+          });
+      });
+  }
 };
 
 
 function getVersionNumberArray (version, depth, delimiter) {
-    version = String(version);
+  version = String(version);
 
-    if (version.indexOf(delimiter) == -1) {
-        return version;
-    } else {
-        version = version.split(delimiter);
+  if (version.indexOf(delimiter) == -1) {
+    return version;
+  } else {
+    version = version.split(delimiter);
 
-        let result = [];
-        for (let i = 0, d = Math.min(depth, version.length); i < d; i++) {
-            result.push(version[i]);
-        }
-
-        return result;
+    let result = [];
+    for (let i = 0, d = Math.min(depth, version.length); i < d; i++) {
+      result.push(version[i]);
     }
+
+    return result;
+  }
 }
 
 function needUpdate (depth = Infinity, delimiter = ".") {
-    if (typeof depth === "string") {
-        delimiter = depth;
-        depth = Infinity;
-    }
+  if (typeof depth === "string") {
+    delimiter = depth;
+    depth = Infinity;
+  }
 
-    let currentVersion = CURRENT_VERSION;
+  let currentVersion = CURRENT_VERSION;
 
-    return Promise.resolve()
-        .then(() => getLatestVersion())
-        .then((latestVersion) => {
-            currentVersion = getVersionNumberArray(currentVersion, depth, delimiter);
-            let latestVersionArr = getVersionNumberArray(latestVersion, depth, delimiter);
+  return Promise.resolve()
+    .then(() => getLatestVersion())
+    .then((latestVersion) => {
+      currentVersion = getVersionNumberArray(currentVersion, depth, delimiter);
+      let latestVersionArr = getVersionNumberArray(latestVersion, depth, delimiter);
 
-            const needed = { isNeeded: true, currentVersion: currentVersion, latestVersion: latestVersion };
-            const notNeeded = { isNeeded: false, currentVersion: currentVersion, latestVersion: latestVersion };
+      const needed = { isNeeded: true, currentVersion: currentVersion, latestVersion: latestVersion };
+      const notNeeded = { isNeeded: false, currentVersion: currentVersion, latestVersion: latestVersion };
 
-            for (let i = 0; i < depth; i++) {
-                if (!latestVersionArr[i] && !currentVersion[i]) {
-                    return Promise.resolve(notNeeded);
-                }
-                if (!currentVersion[i]) {
-                    return Promise.resolve(needed);
-                }
-                if (latestVersionArr[i] > currentVersion[i]) {
-                    return Promise.resolve(needed);
-                }
-            }
-            return Promise.resolve(notNeeded);
-        });
+      for (let i = 0; i < depth; i++) {
+        if (!latestVersionArr[i] && !currentVersion[i]) {
+          return Promise.resolve(notNeeded);
+        }
+        if (!currentVersion[i]) {
+          return Promise.resolve(needed);
+        }
+        if (latestVersionArr[i] > currentVersion[i]) {
+          return Promise.resolve(needed);
+        }
+      }
+      return Promise.resolve(notNeeded);
+    });
 }
 
 export default {
-    getPackageName: () => PACKAGE_NAME,
-    getCurrentBuildNumber: () => CURRENT_BUILD_NUMBER,
-    getCurrentVersion: () => CURRENT_VERSION,
-    getLatestVersion: getLatestVersion,
-    needUpdate: needUpdate
+  getPackageName: () => PACKAGE_NAME,
+  getCurrentBuildNumber: () => CURRENT_BUILD_NUMBER,
+  getCurrentVersion: () => CURRENT_VERSION,
+  getLatestVersion: getLatestVersion,
+  needUpdate: needUpdate
 };
