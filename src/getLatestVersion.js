b3c03a3d936ac7212f7126f7c499a45301b4c346
kimxogus <kgyoo8232@gmail.com>
2016-12-16 21:43:24 +0900

    add option

--- src/getLatestVersion.js
+++ src/getLatestVersion.js
@@ -1,7 +1,9 @@
 /**
  * Created by kimxogus on 2016. 12. 15..
  */
+import { isNil, defaultsDeep } from "lodash";
 import { Platform } from "react-native";
+
 import Native from "./native";
 
 let latestVersion;
@@ -16,7 +18,19 @@ export function setAppID(_appID) {
   appID = _appID;
 }
 
-export function getLatestVersion() {
+export function getLatestVersion(option) {
+  option = defaultsDeep(option, {
+    forceUpdate: false,
+    url: null,
+    fetchOptions: {
+      timeout: 5000
+    }
+  });
+
+  if (isNil(option.url)) {
+    option.url = getStoreUrl(); // To prevent getStore to be executed when it's not needed.
+  }
+
   if (latestVersion) {
     return Promise.resolve(latestVersion);
   } else if (Platform.OS === "android") {
@@ -25,21 +39,25 @@ export function getLatestVersion() {
         latestVersion = version;
         return Promise.resolve(latestVersion);
       })
-      .catch(getLatestVersionFromStore);
+      .catch(() => getLatestVersionFromUrl(option.url, option.fetchOptions));
   } else if (Platform.OS === "ios") {
     return Promise.resolve()
-      .then(getLatestVersionFromStore);
+      .then(() => getLatestVersionFromUrl(option.url, option.fetchOptions));
   }
 }
 
-function getLatestVersionFromStore() {
+function getStoreUrl() {
   if (Platform.OS === "ios" && (!appName || !appID)) {
     throw new Error("'appName' or 'appID' is undefined.\nSet those values correctly using 'setAppName()' and 'setAppID()'");
   }
-  return fetch(Platform.select({
+  return Platform.select({
     android: "https://play.google.com/store/apps/details?id=" + Native.getPackageName(),
     ios: "https://itunes.apple.com/" + Native.getCountry() + "/app/" + appName + "/id" + appID
-  }), { timeout: 5000 })
+  });
+}
+
+function getLatestVersionFromUrl(url, fetchOptions) {
+  return fetch(url, fetchOptions)
     .then(res => {
       return res.text();
     })
