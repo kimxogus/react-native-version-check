c19fab3c3a7a3d2a79056da138863d66d3f74564
kimxogus <kgyoo8232@gmail.com>
2017-06-14 20:43:24 +0900

    remove unused statements

--- scripts/test.js
+++ scripts/test.js
@@ -1,8 +1,6 @@
-import path from 'path';
 import jest from 'jest';
 
 process.env.RNVC_ENV = 'test';
-process.env.HOME = path.join(process.cwd(), 'testHome');
 
 const argv = process.argv.slice(2);
 
