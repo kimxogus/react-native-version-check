a90f049f0071d99d23cae10d121eb982bac4fd09
kimxogus <kgyoo8232@gmail.com>
2017-08-31 21:23:20 +0900

    add getCountryAsync and warning message to expo

--- src/versionInfo.js
+++ src/versionInfo.js
@@ -1,4 +1,5 @@
 let RNVersionCheck;
+const useExpo = !!process.env.RNVC_EXPO;
 if (process.env.RNVC_ENV === 'test') {
   RNVersionCheck = {
     country: 'ko',
@@ -6,7 +7,7 @@ if (process.env.RNVC_ENV === 'test') {
     currentBuildNumber: 1,
     currentVersion: '0.0.1',
   };
-} else if (process.env.RNVC_EXPO) {
+} else if (useExpo) {
   const { Platform } = require('react-native');
   const { Constants, Util } = require('expo');
 
@@ -25,7 +26,7 @@ if (process.env.RNVC_ENV === 'test') {
 
   RNVersionCheck = {
     currentVersion: version,
-    country: (async () => await Util.getCurrentDeviceCountryAsync())(),
+    country: Util.getCurrentDeviceCountryAsync(),
     currentBuildNumber: Platform.select({
       android: versionCode,
       ios: buildNumber
@@ -45,7 +46,13 @@ const CURRENT_BUILD_NUMBER = RNVersionCheck.currentBuildNumber;
 const CURRENT_VERSION = RNVersionCheck.currentVersion;
 
 export default {
-  getCountry: () => COUNTRY,
+  getCountry: () => {
+    if (useExpo) {
+      console.warn('getCountry will return Promise<country>. Use "await getCountry() or await getCountryAsync()"');
+    }
+    return COUNTRY;
+  },
+  getCountryAsync: () => Promise.resolve(COUNTRY),
   getPackageName: () => PACKAGE_NAME,
   getCurrentBuildNumber: () => CURRENT_BUILD_NUMBER,
   getCurrentVersion: () => CURRENT_VERSION,
