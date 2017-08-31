e2e7ec01ea00615111b14a91527a3a66f8808c02
kimxogus <kgyoo8232@gmail.com>
2017-08-31 21:37:59 +0900

    add test for getCountryAsync

--- src/__tests__/versionInfo.js
+++ src/__tests__/versionInfo.js
@@ -3,6 +3,7 @@ import VersionInfo from '../versionInfo';
 describe('Native', () => {
   it('Native variables should match', () => {
     expect(VersionInfo.getCountry()).toBe('ko');
+    VersionInfo.getCountryAsync().then(country => expect(country).toBe('ko'));
     expect(VersionInfo.getPackageName()).toBe('com.reactnative.versioncheck');
     expect(VersionInfo.getCurrentBuildNumber()).toBe(1);
     expect(VersionInfo.getCurrentVersion()).toBe('0.0.1');
