f9fbd3640be945729692c774ef1c10f34e0426ac
kimxogus <kgyoo8232@gmail.com>
2017-09-21 01:08:44 +0900

    move rnvc sources

--- packages/react-native-version-check/index.js
+++ packages/react-native-version-check/index.js
@@ -1,3 +1,25 @@
-import VersionCheck from '../../index';
+import VersionInfo from './src/versionInfo';
+import { getLatestVersion } from './src/getLatestVersion';
+import {
+  getStoreUrl,
+  getStoreUrlAsync,
+  setAppID,
+  setAppName,
+} from './src/providers/store';
+import { needUpdate } from './src/needUpdate';
 
-export default VersionCheck;
+export default {
+  getCountry: VersionInfo.getCountry,
+  getCountryAsync: VersionInfo.getCountryAsync,
+  getPackageName: VersionInfo.getPackageName,
+  getCurrentBuildNumber: VersionInfo.getCurrentBuildNumber,
+  getCurrentVersion: VersionInfo.getCurrentVersion,
+
+  setAppID,
+  setAppName,
+  getStoreUrl,
+  getStoreUrlAsync,
+  getLatestVersion,
+
+  needUpdate,
+};
