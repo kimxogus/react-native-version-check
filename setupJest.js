0ecf5f3fbca4d9f1dd88179220d7ca283d8692e1
kimxogus <kgyoo8232@gmail.com>
2017-09-21 21:13:05 +0900

    fix jest test

--- setupJest.js
+++ setupJest.js
@@ -1,1 +1,3 @@
 require('isomorphic-fetch');
+require('./packages/react-native-version-check/src/versionInfo')
+  .setVersionInfo(require('./packages/react-native-version-check/src/RNVersionInfo'));
