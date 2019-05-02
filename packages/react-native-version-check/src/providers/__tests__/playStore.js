6936131ab549c536e8dc3c18fb24d8de51d7eb31
CarlaTeo <CarlaTeo@users.noreply.github.com>
2019-05-02 14:12:46 -0300

    Change PlayStore crawler hardcoded string to regex
    PayStore pages sometimes suffer from small changes (e.g. a div now receives a class).  This should not be enough to break the crawler, so now it is using a regex to find the Current Version.

---
+++ packages/react-native-version-check/src/providers/__tests__/playStore.js
@@ -0,0 +1,103 @@
+import PlayStoreProvider from '../playStore';
+
+const options = {
+  packageName: "com.myapp",
+}
+
+const mockSuccesfulResponse = ( returnBody ) => {
+  global.fetch = jest.fn().mockImplementationOnce(() => {
+    return new Promise((resolve, reject) => {
+      resolve(new Response(returnBody));
+    });
+  });
+};
+
+describe('PlayStoreProvider get version from older Play Store layouts', () => {
+
+  it('get version from older (since ~Apr, 2018) Play Store layout', async () => {
+    mockSuccesfulResponse("\
+      <html>\
+      <div class=\"hAyfc\">\
+        <div class=\"BgcNfc\">\
+            Current Version\
+        </div>\
+        <span class=\"htlgb\">\
+          <div>\
+            <span class=\"htlgb\">0.10.0</span>\
+          </div>\
+        </span>\
+      </div>\
+      </html>\
+    ")
+
+    await PlayStoreProvider.getVersion(options).then(
+      r => expect(r).toBe("0.10.0")
+    );
+  });
+
+  it('get version from ancient (prior to ~Apr, 2018) Play Store layout', async () => {
+    mockSuccesfulResponse("\
+      <html>\
+      <div class=\"hAyfc\">\
+        <div class=\"BgcNfc\">\
+            Current Version\
+        </div>\
+        <span class=\"htlgb\">\
+            <span class=\"softwareVersion\">0.10.0</span>\
+        </span>\
+      </div>\
+      </html>\
+    ")
+
+    await PlayStoreProvider.getVersion(options).then(
+      r => expect(r).toBe("0.10.0")
+    );
+  });
+
+});
+
+describe('PlayStoreProvider get version current (since ~Dec, 2018) Play Store', () => {
+
+  it('with format x.x.x', async () => {
+    mockSuccesfulResponse("\
+      <html>\
+      <div class=\"hAyfc\">\
+        <div class=\"BgcNfc\">\
+            Current Version\
+        </div>\
+        <span class=\"htlgb\">\
+          <div class=\"IQ1z0d\">\
+            <span class=\"htlgb\">0.10.0</span>\
+          </div>\
+        </span>\
+      </div>\
+      </html>\
+    ")
+
+    await PlayStoreProvider.getVersion(options).then(
+      r => expect(r).toBe("0.10.0")
+    );
+  });
+
+  it('with format xxx', async () => {
+    mockSuccesfulResponse("\
+      <html>\
+      <div class=\"hAyfc\">\
+        <div class=\"BgcNfc\">\
+            Current Version\
+        </div>\
+        <span class=\"htlgb\">\
+          <div class=\"IQ1z0d\">\
+            <span class=\"htlgb\">234</span>\
+          </div>\
+        </span>\
+      </div>\
+      </html>\
+    ")
+
+    await PlayStoreProvider.getVersion(options).then(
+      r => expect(r).toBe("234")
+    );
+  });
+});
+
